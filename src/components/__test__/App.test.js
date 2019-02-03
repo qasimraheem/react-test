bb3e54469c464500426a04384cb668ea594e37ed
qasim raheem mac <qasimmehmood13936@gmail.com>
2019-02-03 12:03:17 +0500

    except in test

--- src/components/__test__/App.test.js
+++ src/components/__test__/App.test.js
@@ -3,10 +3,10 @@ import ReactDOM from 'react-dom';
 import App from '../App';
 
 it('Shows a comment box', () => {
-   const div =document.create('div');
+   const div = document.createElement('div');
    ReactDOM.render(<App/>,div);
 
-   
+   expect(div.innerHTML).toContain('comment box');
 
    ReactDOM.unmountComponentAtNode(div);
 });
