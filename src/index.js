b1b33e62618acca0d33eda531277ac204907072e
qasim raheem mac <qasimmehmood13936@gmail.com>
2019-02-03 10:58:03 +0500

    first commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App';
+
+
+// ReactDOM.render(
+//     <div>
+//         <h1>News</h1>
+//     </div>,
+//     document.getElementById('root')
+// );
+ReactDOM.render(<App />, document.querySelector('#root'));
