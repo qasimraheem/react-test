c73bb0b27f20e0e6d0facc55dc9b1d9d02f1699a
qasim raheem mac <qasimmehmood13936@gmail.com>
2019-02-03 15:02:16 +0500

    enzyme shallow test run

--- src/components/__test__/App.test.js
+++ src/components/__test__/App.test.js
@@ -1,13 +1,18 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+// import { shallow } from 'enzyme';
 import App from '../App';
+import CommentBox from '../CommentBox';
+import { shallow, mount, render } from 'enzyme';
 
 it('Shows a comment box', () => {
-   const div = document.createElement('div');
-   ReactDOM.render(<App/>,div);
+   // const div = document.createElement('div');
+   // ReactDOM.render(<App/>,div);
+   //
+   // expect(div.innerHTML).toContain('comment box');
+   // //  expect(div).toHaveAnInstanceOf(CommentBox);
 
-   expect(div.innerHTML).toContain('comment box');
-   //  expect(div).toHaveAnInstanceOf(CommentBox);
 
-   ReactDOM.unmountComponentAtNode(div);
+   const wrapped = shallow(<App/>);
+   expect(wrapped.find(CommentBox).length).toEqual(1);
+   // ReactDOM.unmountComponentAtNode(div);
 });
