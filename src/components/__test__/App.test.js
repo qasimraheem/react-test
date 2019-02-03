0fa3d0ab6b0452ae7178e70969f2c272f36c4db0
qasim raheem mac <qasimmehmood13936@gmail.com>
2019-02-03 15:09:49 +0500

    second expect shallow

--- src/components/__test__/App.test.js
+++ src/components/__test__/App.test.js
@@ -2,6 +2,7 @@ import React from 'react';
 // import { shallow } from 'enzyme';
 import App from '../App';
 import CommentBox from '../CommentBox';
+import CommentList from '../CommentList';
 import { shallow, mount, render } from 'enzyme';
 
 it('Shows a comment box', () => {
@@ -16,3 +17,9 @@ it('Shows a comment box', () => {
    expect(wrapped.find(CommentBox).length).toEqual(1);
    // ReactDOM.unmountComponentAtNode(div);
 });
+
+//second expect shallow
+it('shows a comment list',()=>{
+   const wrapper=shallow(<App/>);
+   expect(wrapper.find(CommentList).length).toEqual(1);
+})
